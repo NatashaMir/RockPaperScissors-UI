fd0841d268dc4fb768d7a5dd8ef6fd187fba9845
Nataliia Miroshnychenko <nataliia.mirosh@gmail.com>
2023-03-26 21:42:38 +0300

    Create page.tsx and css files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,66 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client';
+import { useState } from 'react';
+import React from 'react';
+import axios from 'axios';
+import styles from "./Game.module.scss";
+import "./table.css";
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Round{
+  firstPlayer : string;
+  secondPlayer : string;
+  gameResult : string;
 }
+
+function GameComponent() {
+  const [gameId, setGameId] = useState(undefined);
+  const [rounds, setRounds] = useState([]);
+
+  const getGame = async () => {
+    const response = await axios.get(`http://localhost:8080/play/${gameId}`);
+    console.log(response);
+    setRounds(response.data.rounds);
+  };
+
+  const restartGame = async () => {
+    const response = await axios.get('http://localhost:8080/restart');
+    console.log(response);
+    setGameId(response.data.gameId);
+    setRounds([]);
+  };
+
+  return (
+    <div className={styles.main}>
+      <label htmlFor="gameId">Game ID: {gameId}</label>
+      <div>      
+        <button onClick={getGame}>Get Game</button>
+        <button onClick={restartGame}>Restart Game</button>
+      </div>
+
+    <div className="table">
+      <table>
+        <thead>
+          <tr>
+            <th>Round</th>
+            <th>First Player</th>
+            <th>Second Player</th>
+            <th>Game result</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rounds.map((round : Round, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{round.firstPlayer}</td>
+              <td>{round.secondPlayer}</td>
+              <td>{round.gameResult}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  );
+}
+
+export default GameComponent;
